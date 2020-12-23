6ea6f4776ba5de68bcd52215914bd9db68bbb63f
ryanrijo2020 <72882021+ryanrijo2020@users.noreply.github.com>
2020-12-23 21:43:57 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+function logout() {
+    window.location="index.html";
+}
+var firebaseConfig = {
+    apiKey: "AIzaSyBNr9ph8vHiPXNd0cbn5nNoNlAoz0AkP6I",
+    authDomain: "project-94-4ff99.firebaseapp.com",
+    projectId: "project-94-4ff99",
+    storageBucket: "project-94-4ff99.appspot.com",
+    messagingSenderId: "892052886304",
+    appId: "1:892052886304:web:249a21a7ba73df713d7d45"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
